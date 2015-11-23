84b57d5d018c181253a030c5497b73393c699b77
0xbadfca11 <0xbadfca11@gmail.com>
2015-11-23 23:27:26 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 $src = "E:\test.vhdx";
 $dest = "E:\test2.vhdx";
 
-foreach($i in @(1GB, 2GB, 3GB ,4GB))
+foreach($i in @(3GB, 5GB, 9GB))
 {
     \\win-56oprnnmqao\tmp\vhdxtool.exe create -s $i -f $src > $null
     if( (Start-Process -FilePath "x64/Debug/reflink.exe" -ArgumentList $src,$dest -NoNewWindow -PassThru -Wait).ExitCode -ne 0 )
@@ -9,7 +9,7 @@ foreach($i in @(1GB, 2GB, 3GB ,4GB))
         throw "reflink fail";
     }
     (Measure-Command{
-    if( [string]::Compare( (Get-FileHash $src -Verbose).Hash, (Get-FileHash $dest -Verbose).Hash ) -ne 0 )
+    if( [string]::Compare( (Get-FileHash $src).Hash, (Get-FileHash $dest).Hash ) -ne 0 )
     {
         throw "file hash mismatch";
     }
