9b6809ccaaf6938526ff92471566a7aa30a3fc02
0xbadfca11 <0xbadfca11@gmail.com>
2018-10-26 20:54:26 +0900

    Update reflink.h

--- reflink.h
+++ reflink.h
@@ -1,7 +1,15 @@
 #pragma once
-#include <winnt.h>
 #include <sal.h>
+#include <stdbool.h>
 
+#ifndef _INC_WINDOWS
+#ifdef __cplusplus
+extern "C"
+#endif
 _Success_(return == true)
+bool reflink(_In_z_ const wchar_t oldpath[], _In_z_ const wchar_t newpath[]);
+#else
 EXTERN_C
+_Success_(return == true)
 bool reflink(_In_z_ PCWSTR oldpath, _In_z_ PCWSTR newpath);
+#endif
