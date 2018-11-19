e83d8a5dd08f5d0003723c9f7d2879442a800d51
0xbadfca11 <0xbadfca11@gmail.com>
2018-11-20 00:58:44 +0900

    Revert

--- reflink.h
+++ reflink.h
@@ -1,15 +1,8 @@
 #pragma once
-#include <sal.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <stdbool.h>
 
-#ifndef _INC_WINDOWS
-#ifdef __cplusplus
-extern "C"
-#endif
-_Success_(return == true)
-bool reflink(_In_z_ const wchar_t oldpath[], _In_z_ const wchar_t newpath[]);
-#else
 EXTERN_C
 _Success_(return == true)
 bool reflink(_In_z_ PCWSTR oldpath, _In_z_ PCWSTR newpath);
-#endif
